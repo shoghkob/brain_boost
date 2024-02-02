db26c6ffd655355f26dd453dc304dd0b7e57e1aa
Shoghine <shoghkobalyan7@gmail.com>
2024-02-03 00:42:14 +0300

    Brain Boost

--- app/src/main/java/com/example/brainboost/mainPage.java
+++ app/src/main/java/com/example/brainboost/mainPage.java
@@ -1,15 +1,12 @@
 package com.example.brainboost;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class mainPage extends AppCompatActivity {
 
-    ImageView profile_icon;
+//    ImageView profile_icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +14,14 @@ public class mainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
 
 
-        profile_icon = findViewById(R.id.user_logo);
-
-        profile_icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(mainPage.this, profilePage.class));
-            }
-        });
+//        profile_icon = findViewById(R.id.user_logo);
+//
+//        profile_icon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(mainPage.this, profilePage.class));
+//            }
+//        });
     }
 
 }
