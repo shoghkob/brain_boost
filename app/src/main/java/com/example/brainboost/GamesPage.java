03e2a0407abba013689a40ae640ba2763691b932
Shoghine <shoghkobalyan7@gmail.com>
2024-02-17 01:30:46 +0300

    Brain Boost

---
+++ app/src/main/java/com/example/brainboost/GamesPage.java
@@ -0,0 +1,14 @@
+package com.example.brainboost;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GamesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_page);
+    }
+}
--- app/src/main/java/com/example/brainboost/mainPage.java
+++ app/src/main/java/com/example/brainboost/mainPage.java
@@ -1,12 +1,17 @@
 package com.example.brainboost;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class mainPage extends AppCompatActivity {
 
-//    ImageView profile_icon;
+    ImageView profile_icon;
+    ImageView first_course;
+    ImageView games;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +19,28 @@ public class mainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
 
 
-//        profile_icon = findViewById(R.id.user_logo);
-//
-//        profile_icon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(mainPage.this, profilePage.class));
-//            }
-//        });
+        profile_icon = findViewById(R.id.profile);
+        first_course = findViewById(R.id.first_course);
+        games = findViewById(R.id.games);
+        profile_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mainPage.this, profilePage.class));
+            }
+        });
+
+        first_course.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mainPage.this, FirstCourse.class));
+            }
+        });
+        games.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mainPage.this, GamesPage.class));
+            }
+        });
     }
 
 }
