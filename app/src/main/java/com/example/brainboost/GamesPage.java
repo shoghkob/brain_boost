a5eb505484d267ddf4dad3d438318dd1ecea83a8
raf2007 <mehrabyanraffi@mail.ru>
2024-04-13 10:44:23 +0400

    updates

--- app/src/main/java/com/example/brainboost/GamesPage.java
+++ app/src/main/java/com/example/brainboost/GamesPage.java
@@ -1,6 +1,9 @@
 package com.example.brainboost;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,30 @@ public class GamesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_page);
+
+        ImageView game1 = findViewById(R.id.game1);
+        ImageView game2 = findViewById(R.id.game2);
+        ImageView game3 = findViewById(R.id.game3);
+        game1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GamesPage.this, AddPlayers.class));
+            }
+        });
+        game2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GamesPage.this, QuizGameStartPage.class));
+            }
+        });
+        game3
+                .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GamesPage.this, AddPlayers.class));
+            }
+        });
     }
+
+
 }
